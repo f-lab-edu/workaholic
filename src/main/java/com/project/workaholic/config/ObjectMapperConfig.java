38e0ded2c98e1603db6191315b38987fab9ccba7
LIBRA-PARK <park.libra1011@gmail.com>
2024-06-25 21:08:44 +0900

    ObjectMapper 타임스탬프 미지원 수정

--- src/main/java/com/project/workaholic/config/ObjectMapperConfig.java
+++ src/main/java/com/project/workaholic/config/ObjectMapperConfig.java
@@ -2,6 +2,8 @@ package com.project.workaholic.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +14,8 @@ public class ObjectMapperConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
     }
 }

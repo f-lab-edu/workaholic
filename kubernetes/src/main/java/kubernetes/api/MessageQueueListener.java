3702a829140e160f6c5e6ce52c09ffd5316045cf
LIBRA-PARK <park.libra1011@gmail.com>
2024-08-20 00:03:35 +0900

    kubernetes 관련 이벤트 세분화

--- kubernetes/src/main/java/kubernetes/api/MessageQueueListener.java
+++ kubernetes/src/main/java/kubernetes/api/MessageQueueListener.java
@@ -11,6 +11,8 @@ import kubernetes.deploy.service.DeployService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 import rabbit.message.queue.ProducerService;
 
@@ -35,13 +37,22 @@ public class MessageQueueListener {
         this.projectPodService = projectPodService;
     }
 
-    @RabbitListener(queues = "workaholic.kubernetes.build", concurrency = "2")
-    public void receiveBuildMessageQueue(Message message) throws IOException {
-        WorkProject workProject = objectMapper.readValue(message.getBody(), WorkProject.class);
-        WorkProjectSetting projectSetting = workProjectService.getSettingByWorkProjectId(workProject.getId());
+    @RabbitListener(queues = "workaholic.kubernetes", concurrency = "2")
+    public void receiveBuildMessageQueue(Message message, @Header(AmqpHeaders.RECEIVED_ROUTING_KEY)String rotingKey) throws IOException {
+        if (rotingKey.equals("workaholic.kubernetes.build")) {
+            WorkProject workProject = objectMapper.readValue(message.getBody(), WorkProject.class);
+            buildDockerImage(workProject);
+        } else if(rotingKey.equals("workaholic.kubernetes.deploy")) {
+            WorkProjectSetting projectSetting = objectMapper.readValue(message.getBody(), WorkProjectSetting.class);
+            deployApplication(projectSetting);
+        }
+    }
 
+    private void buildDockerImage(WorkProject workProject) {
         try {
+            WorkProjectSetting projectSetting = workProjectService.getSettingByWorkProjectId(workProject.getId());
             buildService.buildImage(workProject.getClonePath(), projectSetting);
+
             ProjectPod createdPod = new ProjectPod(workProject.getId());
             projectPodService.createProjectPod(createdPod);
             producerService.sendMessageQueue("workaholic.kubernetes.deploy", projectSetting);
@@ -51,11 +62,8 @@ public class MessageQueueListener {
         }
     }
 
-    @RabbitListener(queues = "workaholic.kubernetes.deploy", concurrency = "2")
-    public void receiveDeployMessageQueue(Message message) throws IOException {
-        WorkProjectSetting projectSetting = objectMapper.readValue(message.getBody(), WorkProjectSetting.class);
+    public void deployApplication(WorkProjectSetting projectSetting) {
         ProjectPod projectPod = projectPodService.getPodByProjectId(projectSetting.getId());
-
         try {
             String address = deployService.deployApplication(projectPod.getNamespace(), projectPod.getName(),
                     "imageName", projectSetting.getTargetPort(), projectSetting.getNodePort());
